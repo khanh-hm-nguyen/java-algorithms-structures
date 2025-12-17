975c119d3471c6663200d2a53f1fefc5018e70b9
Khanh Nguyen <khanh.hm.nguyen03@gmail.com>
2025-12-17 17:53:56 +0700

    find duplicate in array

--- src/main/java/fundamentals/FindDuplicateInArray.java
+++ src/main/java/fundamentals/FindDuplicateInArray.java
@@ -1,5 +1,8 @@
 package fundamentals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Detects if the provided array contains any duplicate elements.
  *
@@ -7,4 +10,25 @@ package fundamentals;
  * @return true if a duplicate exists, false if all elements are unique.
  */
 public class FindDuplicateInArray {
+
+    static boolean hasDuplicate(int[] numbers) {
+
+        if (numbers == null || numbers.length < 1) return false;
+
+        Set<Integer> seenNumbers = new HashSet<>();
+
+        for (int num : numbers) {
+            if (!seenNumbers.add(num)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main (String[] args) {
+        int[] data = {1, 2, 3, 1}; // Should return true
+        System.out.println("Has duplicate: " + hasDuplicate(data));
+    }
+
 }
