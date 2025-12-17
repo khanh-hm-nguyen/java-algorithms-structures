ca24a8f63206219e74c4fdd0d9bbb745ba9a54d1
Khanh Nguyen <khanh.hm.nguyen03@gmail.com>
2025-12-17 18:18:43 +0700

    armstrong number checker basic logic

--- src/main/java/fundamentals/ArmstrongNumberChecker.java
+++ src/main/java/fundamentals/ArmstrongNumberChecker.java
@@ -11,4 +11,35 @@ package fundamentals;
  * @return true if it is an Armstrong number.
  */
 public class ArmstrongNumberChecker {
+
+    public static boolean isAmstrong (int number) {
+
+        int[] digits = intToArr(number);
+
+        int pow = digits.length;
+        int result = 0;
+
+        for (int digit: digits) {
+            result = result + (int) Math.pow(digit, pow);
+
+        }
+
+        return number == result;
+    }
+
+    static int[] intToArr (int numbers) {
+        String temp = String.valueOf(numbers);
+        int[] digits = new int[temp.length()];
+
+        for (int i =0; i < temp.length(); i++) {
+            digits[i] = Character.getNumericValue(temp.charAt(i));
+        }
+        return digits;
+    }
+
+    public static void main (String[] args) {
+        int numbers = 153;
+
+        System.out.println(isAmstrong(numbers));
+    }
 }
